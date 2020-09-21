9b2660a7496485d71acf46e0b1bb8100894ce119
dudumax <bilfinga@gmail.com>
2020-09-21 13:48:30 +0900

    Finish working and refactored palindrome method

--- test/vmaxi_palindrome_test.rb
+++ test/vmaxi_palindrome_test.rb
@@ -1,11 +1,21 @@
 require "test_helper"
 
 class VmaxiPalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::VmaxiPalindrome::VERSION
-  end
+
 
   def test_it_does_something_useful
-    assert false
+    refute "apple".palindrome?
+  end
+
+  def test_literal_palindrome
+    assert "racecar".palindrome?
+  end
+
+  def test_mixed_case_palindrome
+  assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "Madam I'm Adam".palindrome?
   end
 end
